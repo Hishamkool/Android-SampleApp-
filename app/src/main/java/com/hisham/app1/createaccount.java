1071c1bbd030bb77f8b16d8a61cd93443ee49b67
Muhammed hisham <hishamkool@gmail.com>
2022-04-25 22:53:17 +0530

    changes : 0. fixed force stop (reason array type not mentioned in array age. i.e, <String> in declaration and get(i) in Toast were missing )
    1. added spinner for age and state
    
    2. array for age is:
    Arraylist<String> arrayList_Age = new ArrayList();
    arrayList_Age.add("18");
    arrayList_Age.add("< 18");
    arrayList_Age.add("> 18");
    
    Toast.makeText(getApplicationContext(), arrayList_Age.get(i), Toast.LENGTH_LONG).show();
    
    3.Array for states is:
    String states[] ={"jhdjas","jjkdhkj"}
    
    Toast.makeText(getApplicationContext(), states123[i], Toast.LENGTH_LONG).show();

--- app/src/main/java/com/hisham/app1/createaccount.java
+++ app/src/main/java/com/hisham/app1/createaccount.java
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 //import android.widget.EditText;
 
 
 public class createaccount extends AppCompatActivity implements View.OnClickListener {
     String UserName;
     String Password;
+    String states123[] = {"Select state", "Kerala", "Tamil Nadu", "Karanataka", "Andrapradesh"};
+    Spinner st;
+    Spinner age;
+    ArrayAdapter arrayAdapter_selectState;
+    ArrayAdapter arrayAdapter_selectAge;
+    ArrayList<String> arrayList_Age;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,54 @@ public class createaccount extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_createaccount);
         Button submit = (Button) findViewById(R.id.Submit);
         submit.setOnClickListener(this);
+        st = findViewById(R.id.spinner_State);
+        arrayAdapter_selectState = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, states123);
+        st.setAdapter(arrayAdapter_selectState);
+        // to add text view to spinner using built in text view holder(android.R.layout.simple_list_item_1)
+        // We need adaptors to add view
+        //list adapter, array adaptor , base adaptor etc are the examples
+
+        age = findViewById(R.id.spinner_Age);
+        arrayList_Age = new ArrayList();
+        arrayList_Age.add("18");
+        arrayList_Age.add("< 18");
+        arrayList_Age.add("> 18");
+        arrayAdapter_selectAge = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList_Age);
+        age.setAdapter(arrayAdapter_selectAge);
+
+
+        //click on spinner for state
+        // Since its a drop down menu we need setOnItemSelezted listener , but if it was only a list view we
+//       can use setOnItemClickedListerner
+        st.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(getApplicationContext(), states123[i], Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        //spinner for age
+        age.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(getApplicationContext(), arrayList_Age.get(i), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //when noting selection no need for any operation
+
+            }
+        });
+
     }
 
     @Override
@@ -34,10 +94,10 @@ public class createaccount extends AppCompatActivity implements View.OnClickList
         Password = pass.getText().toString();
         i.putExtra("UserName", UserName);
         i.putExtra("Password", Password);
-        if (UserName.matches("") || Password.matches("")  ){
+        if (UserName.matches("") || Password.matches("")) {
             Toast.makeText(this, "Username and password required", Toast.LENGTH_SHORT).show();
 
-        }else {
+        } else {
             startActivity(i);
         }
     }
--- app/src/main/java/com/hisham/app1/home.java
+++ app/src/main/java/com/hisham/app1/home.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class home extends AppCompatActivity implements View.OnClickListener {
+    CheckBox c1, c2, c3, c4;
+    Button check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +21,43 @@ public class home extends AppCompatActivity implements View.OnClickListener {
 
         ImageView homebutton = (ImageView) findViewById(R.id.btnBack);
         homebutton.setOnClickListener(this);
+        c1 = findViewById(R.id.checkBox1);
+        c2 = findViewById(R.id.checkBox2);
+        c3 = findViewById(R.id.checkBox3);
+        c4 = findViewById(R.id.checkBox4);
+        check= findViewById(R.id.Check);
 
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checkedcheckbox = ((CheckBox) view).isChecked();
+                String lang = "";
+                switch (view.getId()) {
+                    case R.id.checkBox1:
+                        lang = checkedcheckbox ? "Malayalam selected" : "Malayalam disselected";
+                        break;
+                    case R.id.checkBox2:
+
+                        lang = checkedcheckbox ? "Tamil selected" : "Tamil diselected";
+                        break;
+                    case R.id.checkBox3:
+
+                        lang = checkedcheckbox ? "Hindi selected" : "Hindi disselected";
+                        break;
+                    case R.id.checkBox4:
+                        lang = checkedcheckbox ? "English selected" : "English disselected";
+                        break;
+
+
+                }
+                Toast.makeText(getApplicationContext(), "Languages Known" + lang, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View backbtn) {
         Intent newinten = new Intent(home.this, login_page.class);
-        startActivity(newinten );
+        startActivity(newinten);
     }
 }
